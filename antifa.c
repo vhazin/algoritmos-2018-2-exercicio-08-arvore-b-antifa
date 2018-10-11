da9248d5968a814ff99e103a683408c5337d998b
Thiago <tjcm@cesar.school>
2018-10-11 11:44:22 -0300

    codigo comentado e explicado

--- antifa.c
+++ antifa.c
@@ -36,28 +36,28 @@ Node * insert(Node * no, int value,int G){  // função para inserir que recebe 
         }
     }
     
-    if(newNode->n == G){
-        Node * lNode = createNode(G);
-        Node * rNode = createNode(G);
+    if(newNode->n == G){  // verifica se o número de chaves é o máximo
+        Node * lNode = createNode(G);  // cria um novo nó a esqueda
+        Node * rNode = createNode(G);  // cria um novo nó a direita
         
-        int middle = newNode->n/2;
-        for(int i = 0; i <middle;i++){
+        int middle = newNode->n/2;  // id do meio do nó de chaves
+        for(int i = 0; i <middle;i++){  // preenche o array de chaves esquerdo
             lNode->keys[i] = newNode->keys[i];
         }
         
-        for(int i = 0;i<middle;i++){
+        for(int i = 0;i<middle;i++){  // preenche o array de chaves direito
             rNode->keys[i] = newNode->keys[middle+1];
         }
         
-        if(newNode->pai == NULL){
+        if(newNode->pai == NULL){  // se não tiver pai, sobe o valor identificado como meio para um nó acima
             newNode->keys[0] = newNode->keys[middle];
-            newNode->childs[0] = lNode;
+            newNode->childs[0] = lNode;  // os filhos vão ser os dois nós esquerdo e direito
             lNode->pai = newNode;
             newNode->childs[1] = rNode;
             rNode->pai = newNode;
             newNode->folha = 0;
             
-        }else{
+        }else{  // se tiver pai, adiciona o valor no array de keys e identifica os dois novos nós como filhos
             newNode->pai->keys[no->pai->n] = newNode->keys[middle];
             newNode->pai->childs[no->pai->n-1] = lNode;
             newNode->pai->childs[no->pai->n] = rNode;
