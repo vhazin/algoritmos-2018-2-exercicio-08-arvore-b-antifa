a9814d57e6b6f6a78c6243a38c8787bcc2872e53
João Victor Pessoa <jvpessoa10@hotmail.com>
2018-10-15 19:52:58 -0300

    Insersão em primeiro filho concluida

--- antifa.c
+++ antifa.c
@@ -25,46 +25,34 @@ Node * insert(Node * no, int value,int G){  // função para inserir que recebe 
     
     if(no->folha == 1){ // verifica se o no a ser inserido é folha
         no->keys[no->n] = value;   // se for folha insere a chave
-        printf("%d",no->keys[no->n]);
+        
         no->n += 1;   // incrementa o numero de chaves atualmente no nó
     }else{  // se não for um nó folha...
+        if(value>newNode->keys[G-1]){
+            newNode->childs[G] = insert(newNode->childs[G],value, G);
+        }
         for(int i = 0 ; i < G;i++){
             if(value<no->keys[i]){
-                newNode = insert(newNode,value, G); // recursividade para inserir na folha dentro do nó chave
+                newNode->childs[i] = insert(newNode->childs[i],value, G); // recursividade para inserir na folha dentro do nó chave
                 break;
             }
         }
+        
     }
     
-    if(newNode->n == G){  // verifica se o número de chaves é o máximo
-        Node * lNode = createNode(G);  // cria um novo nó a esqueda
-        Node * rNode = createNode(G);  // cria um novo nó a direita
-        
-        int middle = newNode->n/2;  // id do meio do nó de chaves
-        for(int i = 0; i <middle;i++){  // preenche o array de chaves esquerdo
-            lNode->keys[i] = newNode->keys[i];
-        }
-        
-        for(int i = 0;i<middle;i++){  // preenche o array de chaves direito
-            rNode->keys[i] = newNode->keys[middle+1];
-        }
-        
-        if(newNode->pai == NULL){  // se não tiver pai, sobe o valor identificado como meio para um nó acima
-            newNode->keys[0] = newNode->keys[middle];
-            newNode->childs[0] = lNode;  // os filhos vão ser os dois nós esquerdo e direito
-            lNode->pai = newNode;
-            newNode->childs[1] = rNode;
-            rNode->pai = newNode;
-            newNode->folha = 0;
-            
-        }else{  // se tiver pai, adiciona o valor no array de keys e identifica os dois novos nós como filhos
-            newNode->pai->keys[no->pai->n] = newNode->keys[middle];
-            newNode->pai->childs[no->pai->n-1] = lNode;
-            newNode->pai->childs[no->pai->n] = rNode;
-        }
-        
-    }
     return newNode;
+}
+
+void printTree(Node * no){
+    for(int i = 0; i<no->n;i++){
+        printf("%d",no->keys[i]);
+    }
+    printf("\n");
+    if(no->folha == 0){
+        for(int i = 0; i<(no->n+1);i++){
+            printTree(no->childs[i]);
+        }
+    }
 }
 
 int main(void){
@@ -82,7 +70,7 @@ int main(void){
         printf("Digite o elemento para inserir >>");
         scanf("%d",&value);
         masterNode =  insert(masterNode,value,G);
-        
+        printTree(masterNode);
         
     }
     
